9e040d524acc0530e57ecaeed9882eb2a090d20e
Elyse Turpin <turpin.elyse@gmail.com>
2022-12-09 08:08:42 -0500

    moved all files from museums to museums_api, because this is where remote appears to be

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# museums_api
-a small Ruby on Rails application providing an endpoint which takes a GPS latitude and
-longitude and spits out the names of museums around that location grouped by their postcode as JSON
+A small Rails app which takes a GPS latitude and longitude and spits out the names of museums around that location grouped by their postcode as JSON.
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::API
+
+  rescue_from StandardError, with: :internal_server_error
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def internal_server_error(exception)
+    if Rails.env.development?
+      response = { type: exception.class.to_s, error: exception.message }
+    else
+      response = { error: 'Internal Servor Error' }
+    end
+    render json: response, status: :internal_server_error
+  end
+end
